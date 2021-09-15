762ee840697851d01a34278f60f121d4a3e1d0d2
Rhett-wang-888 <120445143@qq.com>
2021-09-15 17:02:21 +0800

    Initial commit

--- algo/RemoveNthFromEnd.go
+++ algo/RemoveNthFromEnd.go
@@ -22,3 +22,15 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	cur.Next = cur.Next.Next
 	return dummy.Next
 }
+
+func removeNthFromEnd1(head *ListNode, n int) *ListNode {
+	nodes := []*ListNode{}
+	dummy := &ListNode{0, head}
+
+	for node := dummy; node != nil; node = node.Next {
+		nodes = append(nodes, node)
+	}
+	prev := nodes[len(nodes)-1-n]
+	prev.Next = prev.Next.Next
+	return dummy.Next
+}
