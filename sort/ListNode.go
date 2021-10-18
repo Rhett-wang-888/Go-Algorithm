1f245c6c83d1f7e34626c396d01339f2646d604e
Rhett-wang-888 <120445143@qq.com>
2021-10-18 16:56:39 +0800

    Initial commit

---
+++ sort/ListNode.go
@@ -0,0 +1,18 @@
+/**
+ * @description: TODO
+ * @author rhett.wang
+ * @date 2021/10/18 16:53
+ * @version 1.0
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello word")
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
