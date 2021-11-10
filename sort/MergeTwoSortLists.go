ea56deabbafab78ce03936041c2ffb46668ee199
Rhett-wang-888 <120445143@qq.com>
2021-11-10 10:12:56 +0800

    Initial commit

--- sort/MergeTwoSortLists.go
+++ sort/MergeTwoSortLists.go
@@ -158,3 +158,35 @@ func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 	return dummy.Next
 }
+
+func mergeTwoListS4(l1 *ListNode, l2 *ListNode) *ListNode {
+	prehead := &ListNode{0, nil}
+
+	prev := prehead
+
+	for {
+		if l1 == nil {
+			break
+		}
+		if l2 == nil {
+			break
+		}
+
+		if l1.Val <= l2.Val {
+			prev.Next = l1
+			l1 = l1.Next
+		} else {
+			prev.Next = l2
+			l2 = l2.Next
+		}
+		prev = prev.Next
+
+	}
+	if l1 == nil {
+		prev.Next = l2
+	}
+	if l2 == nil {
+		prev.Next = l1
+	}
+	return prehead.Next
+}
