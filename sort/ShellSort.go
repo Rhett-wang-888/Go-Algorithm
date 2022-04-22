4296a2b449074f2db1c6f5a02330359c02d7a7e8
Rhett-wang-888 <120445143@qq.com>
2022-04-22 10:23:32 +0800

    Initial commit

--- sort/ShellSort.go
+++ sort/ShellSort.go
@@ -15,6 +15,20 @@ func main() {
 		print(b[i], " ")
 	}
 }
+
+func shellSortArray1(nums []int) []int {
+	n := len(nums)
+	for i := 1; i < n; i++ {
+		tmp := nums[i]
+		j := i - 1
+		for j >= 0 && nums[j] > tmp {
+			nums[j+1] = nums[j]
+			j--
+		}
+		nums[j+1] = tmp
+	}
+	return nums
+}
 func shellsortArray(nums []int) []int {
 	gap := len(nums) / 2
 
